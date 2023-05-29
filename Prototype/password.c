18f4951bf6147c9b1c565d7ff8b408353bf09834
Tinu205 <dazleroger404@gmail.com>
2023-05-29 20:34:26 +0530

    password.c

---
+++ Prototype/password.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int main() {
+    char username[MAX];
+    char password[MAX];
+    char usr[16] = "Panda", pass[11] = "Who-cares";
+
+    printf("Welcome to Repo stack prototype\n");
+    printf("Enter your username: ");
+    scanf("%s", username);
+
+    printf("Enter your password: ");
+    scanf("%s", password);
+
+    if (strcmp(username, usr) == 0 && strcmp(password, pass) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+    return 0;
+}
