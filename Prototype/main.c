8c2abbc3feb3dc73aa73cbf6e4e8af3ce1716923
Tinu205 <dazleroger404@gmail.com>
2023-06-01 19:01:33 +0530

    Edited adding the patient feature and reading the data feature, started editing the patient feature

--- Prototype/main.c
+++ Prototype/main.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define max 1000
 #define vmax 100000
 
+//defining the struct
 typedef struct {
-        char name[max];
-        int id;
-        int age;
-        char Doctors[max];
-        char Medic_hist[vmax];
-}hooman;//dummy info data struc
+    char name[max];
+    int id;
+    int age;
+    char Doctors[max];
+    char Medic_hist[vmax];
+} hooman;
 
-void add_pat(){
-    
+//Adding the patient
+void add_pat() {
     hooman panda;
-    FILE *fp;
-    fp = fopen("patient_record.bin","ab");
+    FILE* fp;
+    fp = fopen("patient_record.bin", "ab");
 
     if (fp == NULL) {
         printf("Error opening file.\n");
@@ -25,90 +26,114 @@ void add_pat(){
     }
 
     printf("Enter name of the patient: ");
-    scanf("%s",panda.name);
+    scanf("%s", panda.name);
 
     printf("Enter id of the patient: ");
-    scanf("%d",&panda.id);
+    scanf("%d", &panda.id);
 
     printf("Enter age of the patient: ");
-    scanf("%d",&panda.age);
+    scanf("%d", &panda.age);
 
-    printf("Enter the Doctor name seperated by space: ");
-    scanf(" %[^\n]",panda.Doctors);
+    printf("Enter the Doctor name separated by space: ");
+    scanf(" %[^\n]", panda.Doctors);
 
-    printf("Enter the medical history seperated by spaces: ");
-    scanf(" %[^\n]",panda.Medic_hist);
+    printf("Enter the medical history separated by spaces: ");
+    scanf(" %[^\n]", panda.Medic_hist);
 
-    fprintf(fp, "%s %d %d %s %s\n", panda.name, panda.id, panda.age, panda.Doctors, panda.Medic_hist);
+    // Check if patient ID already exists
+    hooman temp;
+    fseek(fp, 0, SEEK_SET);
+    while (fread(&temp, sizeof(hooman), 1, fp) == 1) {
+        if (temp.id == panda.id) {
+            printf("Patient with ID %d already exists.\n", panda.id);
+            fclose(fp);
+            return;
+        }
+    }
 
     fwrite(&panda, sizeof(hooman), 1, fp);
     fclose(fp);
-    
 }
-//work here tomorrow !!
-void read_dat(){
+
+//Reads the patient data
+void read_dat() {
     system("clear");
-    FILE *fp;
-    fp = fopen("patient_record.bin","rb");
-    
+    FILE* fp;
+    fp = fopen("patient_record.bin", "rb");
+
     char name[max];
     int id;
 
     printf("Enter the name of the patient: \n");
-    scanf(" %[^\n]",name);
+    scanf(" %[^\n]", name);
 
     printf("Enter the id: ");
-    scanf("%d",&id);
+    scanf("%d", &id);
 
     hooman panda;
-    //reads the file
+    // reads the file
     while (fread(&panda, sizeof(hooman), 1, fp) == 1) {
-        if ((strcmp(panda.name,name) == 0) && (panda.id==id)) {
+        if ((strcmp(panda.name, name) == 0) && (panda.id == id)) {
             printf("Name: %s\n", panda.name);
-            printf("Age: %d\n", panda.age);
             printf("Id: %d\n", panda.id);
+            printf("Age: %d\n", panda.age);
+            printf("Doctors: %s\n", panda.Doctors);
+            printf("Medical History: %s\n", panda.Medic_hist);
             printf("\n");
+        }
+        else if(strcmp(panda.name,name) != 0){
+            printf("Not found");
         }
     }
     fclose(fp);
+}
+//Edits the patient data
+void edit_pat(){
+
+    char name[max];
+    int id;
+    FILE* fp;
+
+    scanf(" %[^\n]",name);
+    scanf("%d",&id);
+
+    fp = fopen("patient_record.bin","rb+");
 
 }
-int main(){
-    
+
+int main() {
     int valid = system("./password");
-        
-    while(1)//checks login credentials
-    {
-        system("clear");
-        if(valid != 0){
-            printf("Login successful \n");
-            break;
-        }else{
-            printf("Invalid password/username\n");
-            system("./password");
+
+    system("clear");
+    if (valid == 0) {
+        printf("Invalid password/username\n");
+        system("./password");
+    }
+    else {
+        printf("Login successful \n");
+
+        while (1) {
+            printf("Choose an option\n");
+            printf("1) Add patient\n2) Edit existing patient\n3) Read data\n4) Exit\n");
+            int choice;
+            scanf("%d",&choice);
+
+            switch (choice) {
+                case 1:
+                    system("clear");
+                    add_pat();
+                    break;
+                case 3:
+                    system("clear");
+                    read_dat();
+                    break;
+                default:
+                    break;
+            }
+            if (choice == 4) {
+                break;
+            }
         }
     }
-    while(1)//Actions
-    {
-        system("clear");
-        printf("Choose an option\n");
-        printf("1)Add patient\n2)Edit existing patient\n3)Read data\n4)Exit\n");
-        int choise;
-        scanf("%d",&choise);
-        switch (choise)
-        {
-        case 1:
-            system("clear");
-            add_pat();
-            break;
-        case 3:
-            system("clear");
-            read_dat();
-        default:
-            break;
-        }
-        if(choise == 4){
-            break;
-        }
-    }
+    return 0;
 }
